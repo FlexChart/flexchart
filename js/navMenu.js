d6b930576cfdd05d73a30618fff9eea2747048f4
HannWeb <hanner.a.oviedo@hannwebsites.com>
2021-03-05 22:37:57 -0600

    fix: show and hide navbar on resizing.

--- js/navMenu.js
+++ js/navMenu.js
@@ -1,8 +1,26 @@
+let navbar = document.getElementById("main-navbar")
 let menuButton = document.getElementById("menu-button");
 let aboutPageButton = document.getElementById("about-page");
 let projectsPageButton = document.getElementById("projects-page");
 let loginPageButton = document.getElementById("login-page");
 let menuIsOpened = false;
+
+window.onload = showNavBar;
+window.onresize = showNavBar;
+
+function showNavBar() {
+    if (navbar.clientWidth == 1160) {
+        aboutPageButton.style.display = "block";
+        projectsPageButton.style.display = "block";
+        loginPageButton.style.display = "block";
+    } else {
+        aboutPageButton.style.display = "none";
+        projectsPageButton.style.display = "none";
+        loginPageButton.style.display = "none";
+        menuButton.innerHTML = "menu";
+        menuButton.style.backgroundColor = "#FDD17A";
+    }
+}
 
 menuButton.addEventListener("click", () => {
     if (!menuIsOpened) {
@@ -21,6 +39,4 @@ menuButton.addEventListener("click", () => {
         menuButton.style.backgroundColor = "#FDD17A";
 
     };
-
-
 })
