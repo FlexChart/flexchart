be0fc8fd471258996f72c053d45f8e7c10c0b33b
Aaron <baokou66@gmail.com>
2021-03-06 15:13:26 -0500

    add the menu (not fully work)

--- js/navMenu.js
+++ js/navMenu.js
@@ -1,43 +1,32 @@
-let navbar = document.getElementById("main-navbar")
 let menuButton = document.getElementById("menu-button");
-let aboutPageButton = document.getElementById("about-page");
-let projectsPageButton = document.getElementById("projects-page");
-let loginPageButton = document.getElementById("login-page");
+let menuPanel = document.querySelector(".menu-panel");
 let menuIsOpened = false;
 
-window.onload = showNavBar;
-window.onresize = showNavBar;
 
-function showNavBar() {
-    if (navbar.clientWidth == 1160) {
-        aboutPageButton.style.display = "block";
-        projectsPageButton.style.display = "block";
-        loginPageButton.style.display = "block";
-    } else {
-        menuIsOpened = false;
-        aboutPageButton.style.display = "none";
-        projectsPageButton.style.display = "none";
-        loginPageButton.style.display = "none";
+window.onload = showMenuPanel;
+window.onresize = showMenuPanel;
+
+
+function showMenuPanel() {
+    if (menuPanel.clientWidth == 1160) {
+        menuPanel.style.display = "none";
+        menuPanel.style.display = "none";
         menuButton.innerHTML = "menu";
         menuButton.style.backgroundColor = "#FDD17A";
     }
 }
 
+
 menuButton.addEventListener("click", () => {
     if (!menuIsOpened) {
         menuIsOpened = true;
-        aboutPageButton.style.display = "block";
-        projectsPageButton.style.display = "block";
-        loginPageButton.style.display = "block";
+        menuPanel.style.display = "block";
         menuButton.innerHTML = "close";
         menuButton.style.backgroundColor = "#ff6666";
     } else {
         menuIsOpened = false;
-        aboutPageButton.style.display = "none";
-        projectsPageButton.style.display = "none";
-        loginPageButton.style.display = "none";
+        menuPanel.style.display = "none";
         menuButton.innerHTML = "menu";
         menuButton.style.backgroundColor = "#FDD17A";
-
     };
 })
---
+++ js/redirect.js
@@ -0,0 +1,1 @@
+location.replace("https://flexchart.github.io/flexchart/public")
