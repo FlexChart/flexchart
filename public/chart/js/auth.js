d3d9bdafeb419f0c580d3f9eb20edcda9fb0916a
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-03-17 18:54:55 -0400

    added layout for users

---
+++ public/chart/js/auth.js
@@ -0,0 +1,23 @@
+firebase.auth().onAuthStateChanged(user=>{
+    if(user){
+        console.log("signed in")
+        window.displayName = user.displayName;
+        window.email = user.email;
+        window.emailVerified = user.emailVerified;
+        window.photoURL = user.photoURL;
+        window.uid = user.uid;
+        window.phoneNumber = user.phoneNumber;
+        window.providerData = user.providerData;
+    }else{
+        unsubscribeProjectsList?.()
+        window.displayName = undefined; 
+        window.email = undefined; 
+        window.emailVerified = undefined; 
+        window.photoURL = undefined; 
+        window.uid = undefined; 
+        window.phoneNumber = undefined; 
+        window.providerData = undefined; 
+        window.location.replace(window.location.origin);
+    }
+})
+
