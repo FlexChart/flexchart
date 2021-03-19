8b4021e92021c5d564c06f53e4c34456261a38c3
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-03-19 18:06:31 -0400

    You can add and delete charts now, and stuff works better

--- public/chart/js/content.js
+++ public/chart/js/content.js
@@ -1,5 +1,6 @@
 window.chartId = window.location.pathname.split('/')[2];
 console.log(chartId)
+let chartName = ""
 
 function updateSidebarList(querySnapshot){
     let items = querySnapshot.docs.map((doc)=>{
@@ -12,4 +13,62 @@ function updateSidebarList(querySnapshot){
             `)
     })
     document.querySelector("#sidebar-list").innerHTML = items.join('  ')
+}
+
+firestore.collection("charts").doc(chartId).onSnapshot((doc)=>{
+    chartName = doc.data().name
+    document.querySelector("#chart-title").innerHTML = doc.data().name;
+},()=>{
+    document.querySelector("#chart-title").innerHTML = "Not Found"
+})
+
+
+function addChart(){
+    let name=prompt("Enter name of chart:", "New Chart").trim();
+    if(name){
+        firestore.collection("charts").add({
+            name: name,
+            owner: uid
+        })
+    }
+}
+
+
+window.addEventListener("load", ()=>{
+    document.body.addEventListener("click", (e)=>{
+        let element = document.querySelector("#chart-info-popup");
+        if(element.style.opacity != 0 && e.target.closest("#chart-info-popup") != element && e.target.closest("#setting-page") != document.querySelector("#setting-page")){
+            element.style.opacity = 0;
+            element.style.pointerEvents = "none"
+        }
+    })
+})
+
+function togglePopup(){
+    let element = document.querySelector("#chart-info-popup");
+    if(element.style.opacity != 1){
+        element.style.opacity = 1;
+        element.style.pointerEvents = "auto"
+    }else{
+        element.style.opacity = 0;
+        element.style.pointerEvents = "none"
+    }
+}
+
+function deleteChart(){
+    if(confirm(`Delete ${chartName}?`)){
+        firestore.collection("charts").doc(chartId).delete();
+        togglePopup();
+    }
+}
+
+function editChartName(){
+    let name = prompt("Enter new name for chart: ", chartName).trim();
+    if(name){
+        firestore.collection("charts").doc(chartId).update({
+            name: name
+        })
+    }
+
+
 }
