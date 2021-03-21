e67ae79eb962fbdea2432db57fd122747d48c20a
HannWeb <hanner.a.oviedo@hannwebsites.com>
2021-03-21 00:03:10 -0600

    add: covid charts with fecth complete

--- public/js/chart_covid.js
+++ public/js/chart_covid.js
@@ -1,108 +1,175 @@
-function lastSevenDays() {
-    var dates = []
-    today = new Date()
-    days = 86400000 //number of milliseconds in a day
-    for (let i = 1; i <= 5; i++) {
-        dayAgo = new Date(today - (i * days))
-        dates.push((dayAgo.toISOString()).slice(0, 10))
+async function covidCharts(graphURL1, graphURL2) {
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    function getLastDates() {
+        const dates = []
+        const today = new Date()
+        const days = 86400000 //number of milliseconds in a day
+        for (let i = 1; i <= 5; i++) {
+            dayAgo = new Date(today - (i * days))
+            dates.push((dayAgo.toISOString()).slice(0, 10))
+        }
+        return dates
     }
-    return dates
-}
-lastSevenDays()
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
+    async function getCovidData() {
+        try {
+            const result = await fetch(graphURL1, requestOptions)
+            data = await result.json()
+            return data.data
+        } catch (err) {
+            console.log("Error:", err)
+        }
+    }
 
-var data = "";
+    async function getCovidDataByCountry() {
+        try {
+            const result = await fetch(graphURL2, requestOptions)
+            data = await result.json()
+            return data.data
+        } catch (err) {
+            console.log("Error:", err)
+        }
+    }
+    const dates = getLastDates()
+    const covidTimeline = await getCovidData()
+    const covidByCountryRaw = await getCovidDataByCountry()
+    const covidByCountryObject = {}
 
-const globalCountChart = async (url) => {
+    // Graph1
+
+    var totalInfected = document.getElementById("total-confirmed")
+    totalInfected.innerHTML = `Total Cases Confirmed ${new Intl.NumberFormat().format(covidTimeline[0].confirmed)}`
+    var chartUpdateAt = document.getElementById("updated-at")
+    chartUpdateAt.innerHTML = `Updated At ${covidTimeline[0].date}`
     var ctx = document.getElementById('covid').getContext('2d');
-    try {
-        const result = await fetch(url, requestOptions)
-        data = await result.json()
-        console.log(data.result)
-        var totalInfected = document.getElementById("total-confirmed")
-        totalInfected.innerHTML = `Total Cases Confirmed ${new Intl.NumberFormat().format(data.result.confirmed)}`
-        var chartUpdateAt = document.getElementById("updated-at")
-        chartUpdateAt.innerHTML = `Updated At ${data.date}`
 
-        var covid = await new Chart(ctx, {
-            type: 'pie',
+    var covid = await new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Recovered', 'Active', 'Deaths'],
+            datasets: [{
+                data: [covidTimeline[0].recovered, covidTimeline[0].active, covidTimeline[0].deaths],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Covid Global View"
+            },
+            legend: {
+                display: true,
+                position: "left",
+            }
+        }
+    });
+
+    // Graph2
+
+    var ctx2 = document.getElementById('covid2').getContext('2d');
+    var covid2 = new Chart(ctx2, {
+        type: 'horizontalBar',
+        data: {
+            labels: dates,
+            datasets: [{
+                data: [covidTimeline[1].active / 1000000,
+                covidTimeline[2].active / 1000000,
+                covidTimeline[3].active / 1000000,
+                covidTimeline[4].active / 1000000,
+                covidTimeline[5].active / 1000000],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+
+                ],
+                label: "Active cases in Millions",
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        offsetGridLines: true
+                    }
+                }]
+            }
+        }
+    });
+
+    //graph3
+
+    const countrySelect = document.getElementById("countries")
+
+    covidByCountryRaw.forEach(country => {
+        //transform object
+        covidByCountryObject[country.name] = country
+        //filling options
+        var countryElement = document.createElement("option")
+        countryElement.value = country.name;
+        countryElement.innerHTML = country.name
+        countrySelect.appendChild(countryElement)
+    });
+
+
+    countrySelect.onchange = () => {
+        var countryData = covidByCountryObject[countrySelect.value]
+        document.getElementById("covidByMarket").remove();
+        var container = document.getElementById("covidByMarketContainer")
+        var canvas = document.createElement("canvas")
+        canvas.setAttribute("id", "covidByMarket")
+        canvas.classList.add("chart-box-bymarket")
+        container.appendChild(canvas)
+        var ctx = document.getElementById("covidByMarket").getContext('2d');
+        var covidByMarket = new Chart(ctx, {
+            type: 'bar',
             data: {
-                labels: ['Recovered', 'In Recovery', 'Deaths'],
+                labels: ["confirmed", "Recovered", "Critical", "Deaths"],
                 datasets: [{
-                    data: [data.result.recovered, data.result.confirmed - data.result.recovered - data.result.deaths, data.result.deaths],
+                    data: [countryData.latest_data.confirmed / 1000,
+                    countryData.latest_data.recovered / 1000,
+                    countryData.latest_data.critical / 1000,
+                    countryData.latest_data.deaths / 1000,
+                    ],
                     backgroundColor: [
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(255, 99, 132, 0.2)',
+                        'rgba(100, 50, 200, 0.2)',
                     ],
                     borderColor: [
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(255, 99, 132, 1)',
+                        'rgba(100, 50, 200, 1)',
                     ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: "Covid Global View"
-                },
-                legend: {
-                    display: true,
-                    position: "left",
-                }
-            }
-        });
-    } catch (error) {
-        console.log("error", error)
-    }
-
-
-}
-
-const lastSevenGlobal = async (url) => {
-    var ctx2 = document.getElementById('covid2').getContext('2d');
-    var dates = []
-    today = new Date()
-    days = 86400000 //number of milliseconds in a day
-    for (let i = 1; i <= 5; i++) {
-        dayAgo = new Date(today - (i * days))
-        dates.push((dayAgo.toISOString()).slice(0, 10))
-    }
-    try {
-
-        const result = await fetch(url + dates[0] + "/" + dates[4], requestOptions)
-        rawData = await result.json()
-        data = [rawData.result]
-        console.log(data)
-        var covid2 = new Chart(ctx2, {
-            type: 'horizontalBar',
-            data: {
-                labels: dates,
-                datasets: [{
-                    data: [50, 55, 60, 65, 70],
-                    backgroundColor: [
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-
-                    ],
-                    borderColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-
-                    ],
-                    label: "Cases confirmed in Millions",
+                    label: "Numbers in Thousands",
                     borderWidth: 1
                 }]
             },
@@ -116,10 +183,11 @@ const lastSevenGlobal = async (url) => {
                 }
             }
         });
-    } catch (error) {
-        console.log("error", error)
     }
+
 }
 
-// globalCountChart("https://covidapi.info/api/v1/global")
-lastSevenGlobal("https://covidapi.info/api/v1/global/")
+
+covidCharts("https://corona-api.com/timeline", "https://corona-api.com/countries")
+
+
