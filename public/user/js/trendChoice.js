ca6b6ef16149a2f8da93f86cab5e3d7d6c65aff1
Aaron <baokou66@gmail.com>
2021-03-25 10:13:01 -0400

    working on the trendChoices.js and finish later

---
+++ public/user/js/trendChoice.js
@@ -0,0 +1,35 @@
+let viewTrendProjectBtn = document.getElementById("view-trend-project");
+let trendProjectTitle = document.getElementById("trend-project-title");
+let refreshTrendBtn = document.getElementById("refresh-trend-section-btn");
+let trendImg = document.getElementById("trend-img");
+let originLink = window.location.origin + "//charts//";
+let relativeImgLocation = "../img/"
+
+let trendlinks = [
+    originLink + "covid",
+    originLink + "programming_languages",
+    originLink + "ide",
+    originLink + "data_bases",
+    originLink + "anime",
+    originLink + "popular_pages"
+]
+
+let trendTitle = [
+    "COVID-19 Data",
+    "Programming Languages",
+    "IDE Ranking",
+    "Popular Data Bases",
+    "Popular Anime 2021",
+    "Popular Websites"
+]
+
+let imgLocation = [
+    relativeImgLocation + "covid.png",
+]
+
+function randomChoice() {
+
+}
+
+
+
