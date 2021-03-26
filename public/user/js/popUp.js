d08bdc6dc95b12ccb8e31a7b386680c0066cbe14
Aaron <baokou66@gmail.com>
2021-03-26 19:46:07 -0400

    starting to make the pop up for deleting and editing items

---
+++ public/user/js/popUp.js
@@ -0,0 +1,15 @@
+let projectSettingPopUp = document.getElementById("project-setting-pop-up");
+let settingBtn = document.getElementById("setting-btn");
+let darkShadow = document.getElementById("dark-shadow");
+
+settingBtn.addEventListener("click", () => {
+    projectSettingPopUp.style.display = "block";
+    darkShadow.style.display = "block";
+})
+
+document.body.addEventListener("click", (e) => {
+    if(e.target == darkShadow) {
+        projectSettingPopUp.style.display = "none";
+        darkShadow.style.display = "none";
+    }
+})
