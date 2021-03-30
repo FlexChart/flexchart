4bd5ab98cebb20c778c0b7fa5c6997452cfc86f0
HannWeb <hanner.a.oviedo@hannwebsites.com>
2021-03-30 00:03:31 -0600

    fix: add responsiveness to about and fix links

--- public/about/about.js
+++ public/about/about.js
@@ -1,4 +1,25 @@
+const menuPanel = document.getElementById("menu-panel")
+var isOpen = false
+
 function backSite() {
     window.location.href = window.location.origin;
 }
 
+function toggleMenuPanel() {
+    isOpen = !isOpen
+    if (isOpen) {
+        menuPanel.style.display = "block"
+    } else {
+        menuPanel.style.display = "none"
+    }
+}
+
+function hideMenuPanel() {
+    if (window.screen.availWidth > 520) {
+        menuPanel.style.display = "none"
+    }
+    isOpen = false
+}
+
+window.onresize = hideMenuPanel;
+window.onload = hideMenuPanel;
