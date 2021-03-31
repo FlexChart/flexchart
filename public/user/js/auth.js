049bd83df38e1f183cd8c78325b0f488941f333c
HannWeb <hanner.a.oviedo@hannwebsites.com>
2021-03-30 23:05:54 -0600

    fix: user name is now showing on the user screen

--- public/user/js/auth.js
+++ public/user/js/auth.js
@@ -1,28 +1,26 @@
 let unsubscribeProjectsList;
-firebase.auth().onAuthStateChanged(user=>{
-    if(user){
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
         console.log("signed in")
         window.displayName = user.displayName;
-        window.addEventListener("load", () => {
-            let greetTitle = document.getElementById("greet-title");
-            greetTitle.innerHTML = "Hello, " + window.displayName;
-        })
+        let greetTitle = document.getElementById("greet-title");
+        greetTitle.innerHTML = "Hello, " + window.displayName;
         window.email = user.email;
         window.emailVerified = user.emailVerified;
         window.photoURL = user.photoURL;
         window.uid = user.uid;
         window.phoneNumber = user.phoneNumber;
         window.providerData = user.providerData;
-        unsubscribeProjectsList = firestore.collection("charts").where("owner","==",uid).onSnapshot(updateProjectsList)
-    }else{
+        unsubscribeProjectsList = firestore.collection("charts").where("owner", "==", uid).onSnapshot(updateProjectsList)
+    } else {
         unsubscribeProjectsList?.()
-        window.displayName = undefined; 
-        window.email = undefined; 
-        window.emailVerified = undefined; 
-        window.photoURL = undefined; 
-        window.uid = undefined; 
-        window.phoneNumber = undefined; 
-        window.providerData = undefined; 
+        window.displayName = undefined;
+        window.email = undefined;
+        window.emailVerified = undefined;
+        window.photoURL = undefined;
+        window.uid = undefined;
+        window.phoneNumber = undefined;
+        window.providerData = undefined;
         window.location.replace(window.location.origin);
     }
 })
